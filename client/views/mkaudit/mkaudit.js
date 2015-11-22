9cd1c308694095bde83384dd96a7ef1b353a601b
David Carel <dmc1228@gmail.com>
2015-11-22 06:42:10 +0200

    adding outine of static pages

---
+++ client/views/mkaudit/mkaudit.js
@@ -0,0 +1,21 @@
+Template.schoolProfile.helpers({
+
+  
+ m inputAttributes: function () {
+    return { 'class': 'easy-search-input', 'placeholder': 'Example: Mandela Primary' };
+  },
+  ecschools: function () {
+    return ECSchools.find({}, { sort: { schoolName: -1 } });
+  },
+  index: function () {
+    return ECSchoolsIndex;
+  },
+  resultsCount: function () {
+    return ECSchoolsIndex.getComponentDict().get('count');
+  },
+  showMore: function () {
+    return false;
+  },
+  renderTmpl: () => Template.renderTemplate
+});
+
